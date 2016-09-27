911153729cd0599f052f650572ffaa58d709893f
jasminejanet <jasminegeorge93@gmail.com>
2016-09-27 13:29:47 +0530

    close #1

--- public/scripts/Table.js
+++ public/scripts/Table.js
@@ -1,59 +1,48 @@
-var headers = ["Name","Quantity","Price"];
-var rows = [["Jas","2","5000"],["Jus","4","10000"],["Geo","6","15000"],["Sug","8","20000"]];
-var HeaderCell = React.createClass({
-    render() {
-        return (
-        	<span className="header-cell">{this.props.data}</span>
-        );
-    }
-});
-var RowsCell= React.createClass({
-    render(){
-        return (
-           <div className="rows-cell">{this.props.data}</div>
-           );
-    }   
-});
-   
-var TableHeader = React.createClass({
-    render() {
-        return (
-        	<span>
-        		{this.props.headers.map(function(data, idx) {
-        			return <HeaderCell data={data}/>;
-        		})}
-        	</span>
-        );
-    }
-});
+var cols = [
+    { key: 'name1', label: 'Name' },
+    { key: 'qty', label: 'Quantity' },
+    { key: 'purQty', label: 'Purchased Quantity' },
+    { key: 'price', label: 'Price' }
+];
 
-var TableRows = React.createClass({
-    render()
-     {
-        return(
-            <span>
-            {
-               this.props.rows.map(function(data, idx){
-                return <RowsCell data={data}/>;
-                })
-            }
-            </span>
-        );
-    }
-});
+var data = [
+    { name1:'Jasmine',qty:5 ,price:5000},
+    { name1:'Justine',price:10000,qty:10},
+    { name1:'Justine',price:10000,qty:10, purQty:5},
+    { name1:'Justine',price:10000,qty:10, purQty:4},
+    { qty:15 ,price:15000,name1:'Jacob'}
+];
+
 var Table = React.createClass({
-    render() {
+    render: function() {
+        var headerComponents = this.getHeaders(),
+            rowComponents = this.generateRows();
         return (
-        	<span>
-        		<TableHeader headers={headers}/>
-        		<span><TableRows rows={rows}/></span>
-            </span>
+            <table>
+                <thead> {headerComponents} </thead>
+                <tbody> {rowComponents} </tbody>
+            </table>
         );
+    },
+
+    getHeaders: function() {
+        var cols = this.props.cols;
+        return cols.map(function(colData) {
+            return <th className='header-cell' key={colData.key}> {colData.label} </th>;
+        });
+    },
+
+    generateRows: function() {
+        var cols = this.props.cols,
+            data = this.props.data;
+        return data.map(function(item) {
+            var cells = cols.map(function(colData) {
+                return <td> {item[colData.key]} </td>;
+            });
+            return <tr className='rows-cell' key={item.id}> {cells} </tr>;
+        });
     }
 });
 
+React.render(<Table cols={cols} data={data}/>, document.getElementById('jas'));
 
-ReactDOM.render(
-  <Table/>,
-  document.getElementById('content')
-);
